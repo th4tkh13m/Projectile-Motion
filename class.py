dca897e2b0c76c4f2ee5c5b50f80677a761abb04
Khiem Ton That <khiemtonthat@gmail.com>
2021-08-28 17:24:29 +0700

    Add some Object's test cases and getXVelocity method of Ball subclass

--- class.py
+++ class.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 class Object():
-    def __init__(self, name, x, y, m) -> None:
+    def __init__(self, name: str, x: float,
+                 y: float, m: float) -> None:
+        assert m >= 0, "Mass must be greater than 0!"
         self.name = name
         self.x = x
         self.y = y
-        assert m >= 0
         self.m = m
 
     def getName(self) -> str:
@@ -18,23 +21,29 @@ class Object():
     def getM(self) -> float:
         return self.m
 
-    def setX(self, x) -> None:
+    def setX(self, x: float) -> None:
         self.x = x
 
-    def setY(self, y) -> None:
+    def setY(self, y: float) -> None:
         self.y = y
 
-    def setX(self, m) -> None:
+    def setM(self, m: float) -> None:
+        assert m >= 0, "Mass must be greater than 0!"
         self.m = m
 
     def __str__(self) -> str:
-        return "Object: " + self.getName + '\n' \
-                + "Mass: " + self.getM + '\n' \
-                + "Coordinates: <" + str(self.getX) + "," \
-                + str(self.getY) + ">"
+        return "Object: " + str(self.getName()) + '\n' \
+                + "Mass: " + str(self.getM()) + '\n' \
+                + "Coordinates: <" + str(self.getX()) + "," \
+                + str(self.getY()) + ">"
+ 
     def __repr__(self) -> str:
-        return str((self.getName, self.getM,
-                    self.getX, self.getY))
+        return str((self.getName(), self.getM(),
+                    self.getX(), self.getY()))
+## TEST CLASS ##
+# Ball = Object("Ball", 0, 0, -100)
+# print(Ball.getX())
+# print(Ball)
     
 class Ball(Object):
     def __init__(self, name, x, y, m, v, theta) -> None:
@@ -47,5 +56,9 @@ class Ball(Object):
 
     def getAngle(self) -> float:
         return self.angle
+
+    def getXVelocity(self) -> float:
+        return self.getVelocity() \
+                * np.cos(self.getAngle())
 
     
