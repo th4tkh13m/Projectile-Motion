c405a8e555fb874ee308d8358a1b27dacec91564
th4tkh13m <khiemtontht@gmail.com>
2021-09-07 21:47:24 +0700

    Format some codes

--- class.py
+++ class.py
@@ -1,20 +1,20 @@
 import numpy as np
 
+
 class Object():
-    """## Demonstrate the objects on the 2D space ##
-    Include the name, x,y-coordinates and mass of the object.
-    ### Parameters ###
-        name: str. (v >= 0)
-            The name of the Object.
-        x: float
-            The x-coordinate of the Object. 
-        y: float
-            The y-coordinate of the Object. 
-        m: float (m >= 0)
-            The mass of the Object (kg). 
+    """## Demonstrate the objects on the 2D space ##
+    Include the name, x,y-coordinates and mass of the object.
+    ### Parameters ###
+        name: str. (v >= 0)
+            The name of the Object.
+        x: float
+           The x-coordinate of the Object. 
+        y: float
+            The y-coordinate of the Object. 
+        m: float (m >= 0)
+            The mass of the Object (kg). 
     """
-    def __init__(self, name: str, x: float,
-                 y: float, m: float) -> None:
+    def __init__(self, name: str, x: float, y: float, m: float) -> None:
         assert m >= 0, "Mass must be greater than 0!"
         self.name = name
         self.x = x
@@ -22,85 +22,83 @@ class Object():
         self.m = m
 
     def getName(self) -> str:
-        """Return the name of the object 
-        ### Parameters: ###
-            None \n
-        ### Return: ###
-            name: str
-                The name of the object.
+        """Return the name of the object 
+        ### Parameters: ###
+            None \n
+        ### Return: ###
+            name: str
+                The name of the object.
         """
         return self.name
 
     def getX(self) -> float:
-        """Return the x-coordinate of the object 
-        ### Parameters: ###
-            None \n
-        ### Return: ###
-            x: float
-                The current x-coordinate of the object.
+        """Return the x-coordinate of the object 
+        ### Parameters: ###
+            None \n
+        ### Return: ###
+            x: float
+                The current x-coordinate of the object.
         """
         return self.x
-    
+
     def getY(self) -> float:
-        """Return the y-coordinate of the object 
-        ### Parameters: 
-            None \n
-        ### Return: 
-            y: float
-                The current y-coordinate of the object.
+        """Return the y-coordinate of the object 
+        ### Parameters: 
+            None \n
+        ### Return: 
+            y: float
+                The current y-coordinate of the object.
         """
         return self.y
 
     def getM(self) -> float:
-        """Return the mass of the object 
-        ### Parameters: 
-        None
-        ### Return: 
-        m: float
-            The mass of the object (kg).
+        """Return the mass of the object 
+        ### Parameters: 
+        None
+        ### Return: 
+        m: float
+            The mass of the object (kg).
         """
         return self.m
 
     def setX(self, x: float) -> None:
-        """Set the x-coordinate of the object 
-        ### Parameters: 
-        x: float
-            The x-coordinate of the object \n
-        ### Return: 
-            None
+        """Set the x-coordinate of the object
+        ### Parameters:
+        x: float
+            The x-coordinate of the object \n
+        ### Return:
+            None
         """
         self.x = x
 
     def setY(self, y: float) -> None:
-        """Set the y-coordinate of the object 
-        ### Parameters: 
-            y: float
-                The y-coordinate of the object. \n
-        ### Return: 
-            None
+        """Set the y-coordinate of the object
+        ### Parameters:
+            y: float
+                The y-coordinate of the object. \n
+        ### Return:
+            None
         """
         self.y = y
 
     def setM(self, m: float) -> None:
-        """Set the x-coordinate of the object 
-        ### Parameters: 
-            m: float
-                The mass (kg) of the object. Mass must be greater than or equal to 0. \n
-        ### Return: 
-            None
+        """Set the x-coordinate of the object
+        ### Parameters:
+            m: float
+                The mass (kg) of the object. Mass must be greater than or equal to 0. \n
+        ### Return:
+            None
         """
         assert m >= 0, "Mass must be greater than 0!"
         self.m = m
 
     def __str__(self) -> str:
-        return "Object: " + str(self.getName()) + '\n' \
-                + "Mass: " + str(self.getM()) + '\n' \
-                + "Coordinates: <" + str(self.getX()) + "," \
-                + str(self.getY()) + ">"
- 
+        return "Object: " + str(self.getName()) + '\n' + "Mass: " + str(
+            self.getM()) + '\n' + "Coordinates: <" + str(
+                self.getX()) + "," + str(self.getY()) + ">"
+
     def __repr__(self) -> str:
-        return str((self.getName(), self.getM(),
-                    self.getX(), self.getY()))
+        return str((self.getName(), self.getM(), self.getX(), self.getY()))
 
 
 ## TEST CLASS ##
@@ -110,13 +108,13 @@ class Object():
 
 
 class Ball(Object):
-    """Demonstrate the object which is threw at time t = 0.
-    Ball is a subclass of Object class, which has 2 more parameters.
-    ### Parameters
-        v: float. (v >= 0)
-            The magnitude of the initial velocity of the Object.
-        theta: float
-            The angle of the object's initial velocity. 
+    """Demonstrate the object which is threw at time t = 0.
+    Ball is a subclass of Object class, which has 2 more parameters.
+    ### Parameters
+        v: float. (v >= 0)
+            The magnitude of the initial velocity of the Object.
+        theta: float
+            The angle of the object's initial velocity.
     """
     def __init__(self, name, x, y, m, v, theta) -> None:
         super().__init__(name, x, y, m)
@@ -130,11 +128,7 @@ class Ball(Object):
         return self.angle
 
     def getXVelocity(self) -> float:
-        return self.getVelocity() \
-                * np.cos(self.getAngle())
+        return self.getVelocity() * np.cos(self.getAngle())
 
     def getYVelocity(self) -> float:
-        return self.getVelocity() \
-                * np.sin(self.getAngle())
-
-    
+        return self.getVelocity() * np.sin(self.getAngle())
